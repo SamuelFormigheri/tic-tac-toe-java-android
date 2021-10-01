bc58815bd9aa23e7db301951db418845d972cd8e
Samuel Formigheri <samuel.formigheri@hotmail.com>
2021-09-30 23:32:28 -0300

    Adicionado novas telas

---
+++ app/src/main/java/com/example/tictactoe/DashboardActivity.java
@@ -0,0 +1,51 @@
+package com.example.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+    private Button btnExit, btnPlay, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnAbout = (Button) findViewById(R.id.btnAbout);
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(DashboardActivity.this, AboutActivity.class));
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                exitApp(view);
+            }
+        });
+
+
+    }
+
+    public void exitApp(View view) {
+        finish();
+        System.exit(0);
+    }
+
+}
--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
